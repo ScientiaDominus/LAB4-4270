2ab8f0b409221e20168117ead26293371b4c8206
Cameron Young <cky39v@umsystem.edu>
2021-11-09 09:25:47 -0600

    Added lab document for easy access

--- img_prcs.cu
+++ img_prcs.cu
@@ -3,7 +3,10 @@
 
 __global__ void imageProcess()
 {
+    int i = blockIdx.y * blockDim.y + threadIdx.y;
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
 
+    
 }
 
 int main(int argc, char* argv[])
